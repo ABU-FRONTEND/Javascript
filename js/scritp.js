ec37b162621a5ec3e1cc96a3e817c42560de8283
Latipov Abdusamad <abdusamadlatipov05@gmail.com>
2023-05-27 05:15:24 +0300

    feat: for while

---
+++ js/scritp.js
@@ -0,0 +1,115 @@
+// homework 1
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8]
+// let res = 0
+// for(let i = 0; i < arr.length; i++){
+//     res += arr[i]
+// }
+// console.log(res);
+
+//homework 2
+
+
+
+
+// let arr = [1, -2, -5, 4, -3]
+// let res = 0
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] < 0){
+//         res += arr[i]
+//     }
+// }
+// console.log(res);
+
+
+//homework 3
+
+
+
+// let arr = [1, 2, 2, 3, 4, 4, 7, 9, 9, -91, -91]
+// let result = []
+// for(let i = 0; i < arr.length; i++){
+//  for(let num = i + 1; num < arr.length; num++){
+//     if(arr[i] === arr[num]){
+//         console.log(arr[i]);
+//     }
+//  }
+// }
+
+
+
+
+
+// let even = +prompt("sonni kiriting")
+// if(even % 2 === 0){
+//     alert("bu juft son")
+// }
+// else{
+//     alert("bu toq son")
+// }
+
+
+
+
+
+// let weeks = +prompt("hafta kunlarini kiriting")
+// switch (weeks) {
+//     case 1:
+//         alert("bu dushanba")
+//         break;
+//     case 2:
+//         alert("bu seshanba")
+//         break;
+//     case 3:
+//         alert("bu chorshanba")
+//         break;
+//     case 4:
+//         alert("bu payshanba")
+//         break;
+//     case 5:
+//         alert("bu juma")
+//         break;
+//     case 6:
+//         alert("bu shanba")
+//         break;
+//     case 7:
+//         alert('bu yakshanba')
+//         break;
+//     default:
+//         break;
+// }
+
+
+
+// let seasons = +prompt("faslni kiriting")
+// if(seasons === 12 || seasons === 1 || seasons === 2){
+//     alert("qish oyi")
+// }
+// else if(seasons === 3 || seasons === 4 || seasons === 5){
+//     alert("bahor oyi")
+// }
+// else if(seasons === 6 || seasons === 7 || seasons === 8){
+//     alert("yoz oyi")
+// }
+// else if(seasons === 9 || seasons === 10 || seasons === 11){
+//     alert("kuz oyi")
+// }
+// else{
+//     alert("error")
+// }
+
+
+
+// let time = +prompt("enter the time")
+// if(time >= 6 && time <= 12) {
+//     alert("goof morning")
+// }
+// else if(time >= 12 && time <= 18) {
+//     alert("good afternoon")
+// }
+// else if(time >= 18 && time <= 23) {
+//     alert("goof evening")
+// }
+// else{
+//     alert("good night")
+// }
